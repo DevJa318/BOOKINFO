92c8709d225f5d891f5d178f6f3779620b6e05d9
Renata Kipek <rkipek@gmail.com>
2023-03-13 12:48:18 +0100

    tworzy plik json z pobranymi api
    DZIAŁA:
    	pobieranie plików do sprawdzenia
    	pobieranie api
    	pobieranie api jeśli rok jest inny
    	dodawanie api do słownika datadict[book_name] = api
    
    BRAK:
    	dopisywanie brakujących plików

---
+++ getinfo.py
@@ -0,0 +1,56 @@
+import requests
+import json
+
+search1 = '2017 Python Testing with pytest'
+APISEARCH = 'https://api.itbook.store/1.0/search/'
+APIBYISBN = 'https://api.itbook.store/1.0/books/'
+
+def get_json(link,search):
+    url = link + search
+    res = requests.get(url)
+    api = json.loads(res.text)
+    return api
+
+def get_isbn(search, start=0):
+    api = get_json(APISEARCH, search)
+    isbn = api['books'][start]['isbn13']
+    return isbn
+
+def get_data(isbn):
+    api = get_json(APIBYISBN, isbn)
+    return api
+
+if __name__ == '__main__':
+    datafile = open('apidata.txt', 'a')
+    txtfile = 'cleanpythonfiles.txt'
+    datadict = {}
+
+    with open(txtfile) as f:
+        for book_name in f:
+            book_name = book_name.strip()
+            isbn = get_isbn(book_name)
+            data = get_data(isbn)
+            try:
+                if book_name[:4] == data['year'] and book_name[5:15].lower() == data['title'][:10].lower():
+                    print("OK: Pobieram:" + book_name.upper() + "do bazy danych")
+                    datadict[book_name] = data
+                elif book_name[5:15].lower() == data['title'][:10].lower() and int(book_name[:4]) == int(data['year']) or int(book_name[:4]) == int(data['year'])+1 or int(book_name[:4]) == int(data['year'])-1:
+                    print("WARNING: Different year: Pobieram:" + book_name.upper() + "do bazy danych")
+                    datadict[book_name] = data
+                else:
+                    get_isbn(book_name, start=1)
+                    get_data(isbn)
+                    if book_name[:4] == data['year'] and book_name[5:15].lower == data['title'][:10].lower():
+                        print("OK: Pobieram:" + book_name.upper() + "do bazy danych")
+                        print(data)
+                        datadict[book_name] = data
+                    elif book_name[5:15].lower() == data['title'][:10].lower() and int(book_name[:4]) == int(data['year']) or int(book_name[:4]) == int(data['year'])+1 or int(book_name[:4]) == int(data['year'])-1:
+                        print("WARNING: Different year: Pobieram:" + book_name.upper() + "do bazy danych")
+                        datadict[book_name] = data
+                    else:
+                        print('ERROR:' + book_name.upper() + ' dodane do listy brakujących')
+            except:
+                print('NOT INT: ' + book_name)
+
+    datafile.write(json.dumps(datadict))
+    datafile.close()
